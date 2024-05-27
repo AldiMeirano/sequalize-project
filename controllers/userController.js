22539b2295c6dc1606e58a6ee82e53929c3d6606
AldiMeirano <aldymerno173@gmail.com>
2024-05-27 13:40:37 +0100

    feat: create feature bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,14 @@
+const hashPassword = require("../lib/bcrypt");
 const db = require("../models");
 
 const User = db.user;
 
 const registerAccount = async (req, res) => {
   try {
+   
+    const {name, email, password} = req.body
+    const hashedPassword = await  hashPassword(password)
+    req.body.password = hashedPassword
     let info = {
       name: req.body.name,
       email: req.body.email,
