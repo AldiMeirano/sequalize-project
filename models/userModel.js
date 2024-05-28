ccc0db401db13b07ab7921cbfbc6d81c3a0cff22
AldiMeirano <aldymerno173@gmail.com>
2024-05-28 06:30:02 +0100

    feat: create feature refferal code for verification account

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,32 @@
 module.exports = (sequelize, DataTypes) => { 
-    const User = sequelize.define('user', { 
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER
-        },
-        name: { 
-            type : DataTypes.STRING,
-            allowNull: false 
-        },
-        email: { 
-            type : DataTypes.STRING,
-            allowNull: false 
-        },
-        password: { 
-          type : DataTypes.STRING,
-          allowNull: false 
-        },
-     
-    })
+    const User = sequelize.define("user", {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      code_refferal: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: ["verified", "no_verified", "pending"],
+        defaultValue: "pending",
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
     return User
 }
