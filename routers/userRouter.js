a1ae01490d0f729505321362c06b4dc935b46015
Aldi Meirano <116731457+AldiMeirano@users.noreply.github.com>
2024-05-29 10:17:43 +0100

    feat: create feature update picture and upload (#8)

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,6 @@
 const userController = require("../controllers/userController");
 const verifyToken = require("../middleware/jwt");
+const uploader = require("../middleware/uploader");
 
 const router = require("express").Router();
 
@@ -8,5 +9,11 @@ router.post("/login", userController.loginAccount);
 router.post("/verified", userController.verifiedCode);
 router.post("/forgot-password", userController.forgotPassword);
 router.patch("/reset-password", verifyToken, userController.resetPassword);
-
+router.patch("/reset-password", verifyToken, userController.resetPassword);
+router.patch(
+  "/photo-profile",
+  verifyToken,
+  uploader("IMG", "/profile").single("file"),
+  userController.uploadPicture
+);
 module.exports = router;
