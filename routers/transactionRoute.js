4b00407a098685b7afee42b57f580f66893f8616
Aldi Meirano <116731457+AldiMeirano@users.noreply.github.com>
2024-05-31 09:44:36 +0100

    feat: create feature extra time borrow book (#16)

--- routers/transactionRoute.js
+++ routers/transactionRoute.js
@@ -6,4 +6,5 @@ router.get("/:id", transactionController.getDataTransaction);
 router.patch("/penalty", transactionController.getPenalty);
 router.patch("/return-book", transactionController.bookReturner);
 router.post("/search", transactionController.searchBookOrAuthor);
+router.patch("/extratime", transactionController.extraTimeController);
 module.exports = router;
