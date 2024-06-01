1ca4ae711577d334189c3eafe9005ddf78cd9f58
AldiMeirano <aldymerno173@gmail.com>
2024-06-01 06:11:02 +0100

    feat: create feature protect route

--- models/bookModel.js
+++ models/bookModel.js
@@ -13,6 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     author: {
       type: DataTypes.STRING,
       allowNull: false,
--- routers/bookRouter.js
+++ routers/bookRouter.js
@@ -1,7 +1,7 @@
 const bookController = require("../controllers/bookController");
 const router = require("express").Router();
 
-router.post("/add-book", bookController.addNewBook);
+router.post("/add-book/:id", bookController.addNewBook);
 router.get("/:id", bookController.getOneBook);
 router.patch("/:id", bookController.updateDataBook);
 router.delete("/:id", bookController.deleteDataBook);
