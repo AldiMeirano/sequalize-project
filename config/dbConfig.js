19cdacd60ff63add25d3aaa036c89d3deda80353
AldiMeirano <aldymerno173@gmail.com>
2024-06-05 18:27:55 +0100

    config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'crud',
-    dialect: 'mysql',
+  HOST: "128.199.206.234",
+  USER: "root",
+  PASSWORD: "Admin@123456",
+  DB: "crud",
+  dialect: "mysql",
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-}
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
